a99be8b9e09d3ca70fc0af2d970f14885d7b027d
graycrow <kimts2002kr@naver.com>
2016-07-10 00:50:49 +0900

    Created NetManager

---
+++ app/src/main/java/Network/NetManger.java
@@ -0,0 +1,38 @@
+package Network;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Created by graycrow on 2016-07-10.
+ */
+public class NetManger {
+    private void registerPattern() {
+        /*
+        HttpClient httpClient = new DefaultHttpClient();
+        // replace with your url
+        HttpPost httpPost = new HttpPost("www.example.com");
+
+
+        //Encoding POST data
+        try {
+            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePair));
+        } catch (UnsupportedEncodingException e) {
+            // log exception
+            e.printStackTrace();
+        }
+
+        //making POST request.
+        try {
+            HttpResponse response = httpClient.execute(httpPost);
+            // write response to log
+            Log.d("Http Post Response:", response.toString());
+        } catch (ClientProtocolException e) {
+            // Log exception
+            e.printStackTrace();
+        } catch (IOException e) {
+            // Log exception
+            e.printStackTrace();
+        }*/
+
+    }
+}
